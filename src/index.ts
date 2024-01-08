8bbc2b8efd193a757288a839f807ab1e0503b687
Douglas Maia <douglasmai4@protonmail.com>
2024-01-08 17:23:11 -0300

    feat: Express setup and main route

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-console.log('Hello World!')
-console.log("I'm TypeScript!")
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.APP_PORT || 3000;
+
+const app = express()
+
+app.get('/', (_req, res) => {
+  res.status(200).json({ message: 'Hello from Market API' })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port: ${port}`)
+})
